d32b9a5aade732b0214023657a2eb2282f161a8d
JuniorMoura10 <geraldomartins10@gmail.com>
2022-12-05 11:50:41 -0300

    2 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { MenuComponent } from './components/menu.component';
+import { ButtonComponent } from './components/button.component'
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, MenuComponent, ButtonComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/components/button.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'my-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+
+export class ButtonComponent{}
